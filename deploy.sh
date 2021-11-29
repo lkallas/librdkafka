e8ba776c7acbbfa1ca628e3dfd57f2d3708dbd52
Lennar Kallas <lennar@sixfold.com>
2021-11-30 00:17:20 +0200

    more travis ci stuff

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,4 @@
 #Change FTP_ADDRESS with your FTP address. To ensure this works, visit the FTP address in your internet browser to get the right path
-cd artifacts && du -sh -- * && find . -type f -exec curl -v -u $FTP_USER:$FTP_PASS --ftp-create-dirs -T {} ftp://www.lennar.eu/{} \;
+cd artifacts;
+du -sh -- *;
+find . -type f -exec curl --progress-bar -v -u $FTP_USER:$FTP_PASS --ftp-create-dirs -T {} ftp://www.lennar.eu/{} \;
